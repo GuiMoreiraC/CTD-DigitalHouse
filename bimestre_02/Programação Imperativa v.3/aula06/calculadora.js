0acb9b397176ff899cc4ab9da00e15633cb6e127
GuiMoreiraC <guilherme.moreira.c@protonmail.com>
2022-05-11 23:04:09 -0300

    Aula 06

---
+++ bimestre_02/Programação Imperativa v.3/aula06/calculadora.js
@@ -0,0 +1,44 @@
+function adicionar(num1, num2) {
+    return num1 + num2;
+}
+
+function subtracao(num1, num2) {
+    return num1 - num2;
+}
+
+function multiplicacao(num1, num2) {
+    return num1 * num2;
+}
+
+function divisao(num1, num2) {
+    return num1 / num2;
+}
+
+function quadradoDoNumero(num) {
+    return num ** 2;
+}
+
+function calculoPorcentagem(num1, num2) {
+    return multiplicacao(num1, divisao(num2, 100));
+}
+
+function mediaDeTresNumeros(num1, num2, num3) {
+    return divisao(num1 + num2 + num3, 3);
+}
+
+//Testando de Operações
+
+console.log('----------- Teste de Operações / Calculadora -----------');
+
+console.log('Teste - Adicção = ', adicionar(2, 2));
+console.log('Teste - Subitação = ', subtracao(2, 2));
+console.log('Teste - Multiplicação = ', multiplicacao(2, 2));
+console.log('Teste - Divisão = ', divisao(2, 2));
+console.log('Teste - Porcentagem = ', calculoPorcentagem(20, 200));
+console.log('Teste - Quadrado = ', quadradoDoNumero(3));
+console.log('Teste - Média de três numeros = ', mediaDeTresNumeros(7, 5, 4));
+
+/*
+Divisão por zero // Não faça isso!!!
+console.log('Teste - Divisão = ',divisao(2, 0));
+*/

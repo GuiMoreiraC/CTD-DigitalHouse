f91e726a7ea50cf90fcad745c48ebb72c59d0c10
GuiMoreiraC <guilherme.moreira.c@protonmail.com>
2022-06-20 22:50:01 -0300

    21 22

---
+++ bimestre_02/Programação Imperativa v.3/aula22/inde.js
@@ -0,0 +1,12 @@
+// 1) Crie uma função que receba um array composto por números e retorna uma nova matriz (array) com todos os valores divididos pela soma de toda a matriz, usando map() e reduce().
+
+let num = [
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+];
+
+function name(array) {}
+
+num.map();
